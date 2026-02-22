085ab18567e8e06c3db096052f09d5fb36234512
sowon <sowon23sowon23@gmail.com>
2026-02-22 23:40:48 +0900

    Style: place gear admin button in header and add adminBtn styles

--- components/Navigation.js
+++ components/Navigation.js
@@ -23,12 +23,7 @@ export default function Navigation({ onAdminClick }) {
         >
           Gallery
         </Link>
-        <button
-          aria-label="Open admin"
-          onClick={onAdminClick}
-          className={styles.linkBtn}
-          style={{ padding: 8, width: 44, height: 38 }}
-        >
+        <button aria-label="Open admin" onClick={onAdminClick} className={styles.adminBtn}>
           <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M12 15.5A3.5 3.5 0 1 0 12 8.5a3.5 3.5 0 0 0 0 7z" fill="currentColor" />
             <path d="M19.4 15a7.9 7.9 0 0 0 .1-1 7.9 7.9 0 0 0-.1-1l2.1-1.6a.5.5 0 0 0 .12-.64l-2-3.46a.5.5 0 0 0-.6-.22l-2.5 1a8.2 8.2 0 0 0-1.7-.99l-.38-2.65A.5.5 0 0 0 13.3 2h-4.6a.5.5 0 0 0-.5.42l-.38 2.65c-.6.24-1.16.55-1.7.99l-2.5-1a.5.5 0 0 0-.6.22L2.4 8.74a.5.5 0 0 0 .12.64L4.6 11a7.9 7.9 0 0 0-.1 1c0 .34.03.68.1 1L2.4 15.6a.5.5 0 0 0-.12.64l2 3.46a.5.5 0 0 0 .6.22l2.5-1c.54.44 1.1.75 1.7.99l.38 2.65a.5.5 0 0 0 .5.42h4.6a.5.5 0 0 0 .5-.42l.38-2.65c.6-.24 1.16-.55 1.7-.99l2.5 1a.5.5 0 0 0 .6-.22l2-3.46a.5.5 0 0 0-.12-.64L19.4 15z" fill="currentColor" opacity=".9"/>
